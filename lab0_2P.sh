44738e418ae0589559406b7ad538fb3f4c25c5f9
Kitomoori <nopethenik@gmail.com>
2025-12-18 01:44:21 +0300

    Update lab0_2P.sh

--- lab0_2P.sh
+++ lab0_2P.sh
@@ -1,7 +1,7 @@
 chmod 404 lab0/bronzor5
 chmod 664 lab0/crobat0
 
-chmod 440 lab0/delcatty1/exeguttor
+chmod ug+r lab0/delcatty1/exeguttor
 chmod 660 lab0/delcatty1/amoonguss
 chmod 444 lab0/delcatty1/finneon
 chmod 755 lab0/delcatty1
@@ -12,16 +12,16 @@ chmod 400 lab0/dwebble3
 chmod 004 lab0/elgyem2/banette
 chmod 664 lab0/elgyem2/pidgeot
 chmod 666 lab0/elgyem2/eevee
-chmod 335 lab0/elgyem2/sceptile
+chmod ug+wx, o+rx lab0/elgyem2/sceptile
 chmod 357 lab0/elgyem2/chinchou
-chmod 044 lab0/elgyem2/linoone
+chmod go+r lab0/elgyem2/linoone
 chmod 305 lab0/elgyem2
 
 
-chmod 620 lab0/nidorina8/gible
-chmod 573 lab0/nidorina8/drifblim
+chmod u+r, ug+w lab0/nidorina8/gible
+chmod ug+r, go+w, uo+x lab0/nidorina8/drifblim
 chmod 373 lab0/nidorina8/shelgon
-chmod 524 lab0/nidorina8/porygonz
-chmod 444 lab0/nidorina8/clefairy
+chmod uo+r, ug+w lab0/nidorina8/porygonz
+chmod ugo+r lab0/nidorina8/clefairy
 chmod 600 lab0/nidorina8/lotad
-chmod 524 lab0/nidorina8
+chmod uo+r, u+x, g+w lab0/nidorina8
