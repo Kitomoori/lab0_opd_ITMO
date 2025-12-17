c1046173b112d34fb0d299d2fa654e0c3a14c504
Kitomoori <nopethenik@gmail.com>
2025-12-18 01:44:46 +0300

    Update lab0_3P.sh

--- lab0_3P.sh
+++ lab0_3P.sh
@@ -1,14 +1,32 @@
 ln -s lab0/delcatty1 Copy_36
 
+chmod u+r lab0/elgyem2 lab0/elgyem2/sceptile
 rsync -a lab0/elgyem2/ lab0/elgyem2/sceptile/
+chmod u-r lab0/elgyem2 lab0/elgyem2/sceptile
 
+chmod u+rwx lab0/nidorina8 lab0/nidorina8/porygonz lab0/nidorina8/clefairy
 touch lab0/dwebble3_74
 cat lab0/nidorina8/porygonz lab0/nidorina8/clefairy > dwebble3_74
+chmod u-wx lab0/nidorina8/clefairy
+chmod u-x lab0/nidorina8/porygonz
+chmod u-w lab0/nidorina8
 
+chmod u+w lab0/nidorina8
+chmod u+wx lab0/dwebble3
 ln -P lab0/dwebble3 lab0/nidorina8/porygonzdwebble
+chmod u-w lab0/nidorina8
+chmod u-wx lab0/dwebble3
 
+chmod u+x lab0/crobat0
 ln -s lab0/crobat0 lab0/delcatty1/exeggutorcrobat
 
+chmod u+r lab0/elgyem2
 cp lab0/crobat0 lab0/elgyem2/pidgeotcrobat
+chmod u-x lab0/crobat0
+chmod u-r lab0/elgyem2
 
+chmod u+wx lab0/bronzor5 lab0/nidorina8
+chmod u+r lab0/nidorina8/shelgon
 cp lab0/bronzor5 lab0/nidorina8/shelgon
+chmod u-r lab0/nidorina8/shelgon
+chmod u-w lab0/nidorina8
